15032149994bf949662a5992c55d2e1d6d69b288
--replace-all <mandooox@gmail.com>
2015-12-30 16:46:36 +0200

    update watch task

--- gulp/tasks/base/watch.js
+++ gulp/tasks/base/watch.js
@@ -15,7 +15,6 @@ module.exports = function(gulp, $, path, config) {
     	gulp.watch(['./bower_components/**', './bower.json'], [config.task.bower]);
         gulp.watch(path.to.sass.src, [config.task.sass]);
         gulp.watch(path.to.nunjucks.watch, [config.task.nunjucks]);
-        gulp.watch(path.to.js.src.watch, [config.task.scripts]);
     });
 
 };
