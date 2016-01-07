894093494d8f8e10b937ddee27ebb6052f15b476
--replace-all <mandooox@gmail.com>
2016-01-07 12:48:28 +0200

    update paths

--- gulp/paths.js
+++ gulp/paths.js
@@ -11,6 +11,7 @@ var src    = './source/',
 // taks paths
 module.exports = {
     to: {
+    	// dest folders
         dist: {
             dev: dist + dev,
             prod: dist + prod,
@@ -18,11 +19,13 @@ module.exports = {
         },
         dev: dev,
         prod: prod,
+        // nunjucks files
         nunjucks: {
             config: src + 'templates/',
             src: src + 'templates/*.{html,nunjucks}',
             watch: src + 'templates/**/*.+(html|nunjucks)' // for watch task not render
         },
+        // sass files
         sass: {
             src: [
                 src + 'scss/**/*.{scss,sass}',
@@ -37,30 +40,33 @@ module.exports = {
                 prod: dist + prod + assets + 'css'
             }
         },
+        // js files
         js: {
-        	src: {
-	        	main: src + 'js/main.js',
-	        	copy: src + 'js/**/*.js'
-        	},
+            src: {
+                main: src + 'js/main.js',
+                copy: src + 'js/**/*.js'
+            },
             vendor: src + 'js/vendor/',
             dist: {
                 dev: dist + dev + assets + 'js',
                 prod: dist + prod + assets + 'js'
             }
         },
+        // images
         images: {
-        	src: src + 'images/**/*',
-        	logo: src + 'images/logo.png', // change it depend on logo name
-        	dist: {
-        		favicons: dist + dev + assets + 'images/favicons',
+            src: src + 'images/**/*',
+            logo: src + 'images/logo.png', // change it depend on logo name
+            dist: {
+                favicons: dist + dev + assets + 'images/favicons',
                 dev: dist + dev + assets + 'images',
                 prod: dist + prod + assets + 'images'
             }
         },
+        // fonts
         fonts: {
-        	src: src + 'fonts/**/*',
-        	vendor: src + 'fonts/vendor/',
-        	dist: {
+            src: src + 'fonts/**/*',
+            vendor: src + 'fonts/vendor/',
+            dist: {
                 dev: dist + dev + assets + 'fonts',
                 prod: dist + prod + assets + 'fonts'
             }
